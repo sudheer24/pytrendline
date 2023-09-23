7b21f655a97b4020aae86d39bcdef18e88620400
sudheer24 <sudheer424@gmail.com>
2023-09-23 09:37:59 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     description="Detection for support and resistance trendlines on python",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/ednunezg/pytrendline",
+    url="https://github.com/sudheer424/pytrendline",
     license='MIT',
     classifiers=[
         "Programming Language :: Python :: 3",
